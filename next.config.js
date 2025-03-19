efcfb35a88131b72abd89506c3be71d289cfca08
Himaanshu_Sharma <himaanshu.219301657@muj.manipal.edu>
2025-03-19 18:11:01 +0530

    Update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Disable standalone output to avoid symlink issues
-  output: undefined,
+  output: 'standalone',
   webpack: (config) => {
     // Disable symlinks for all bundles
     config.resolve.symlinks = false;
